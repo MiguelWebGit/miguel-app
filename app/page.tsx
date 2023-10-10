bec1ebd6121e4b282c5b21e1cba6c57e3aedbd48
Miguel Guerrero <miguel.dev.cloud@gmail.com>
2023-10-10 19:12:42 -0400

    Create to components, project and projects, I need to at least create a constant for the project props to work

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Projects from "@/components/Projects";
 import Image from "next/image";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
             <div className="flex flex-col items-center justify-center min-h-screen mx-auto max-w-[50rem]">
                 <Hero />
                 <About />
+                <Projects />
             </div>
         </div>
     );
---
+++ components/Project.tsx
@@ -0,0 +1,8 @@
+export interface Project {
+    title: string;
+    image: string;
+    description: string;
+    githubLink: string;
+    liveDemoLink: string;
+  }
+  
