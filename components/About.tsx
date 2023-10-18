716639da19589110d692f2631b2c038c524d5c8a
Miguel Dev <miguel.dev.cloud@gmail.com>
2023-10-18 17:02:40 -0400

    responsive changes

--- components/About.tsx
+++ components/About.tsx
@@ -3,21 +3,23 @@ import Image from "next/image";
 
 const About = () => {
     return (
-        <div id="about" className="grid grid-cols-2 gap-8 items-center mb-40">
-            <div className="flex justify-center">
+        <div
+            id="about"
+            className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center mb-28 md:mb-40 ">
+            <div className="flex justify-center mb-5">
                 <Image
                     className="rounded-lg"
                     src="/about.jpg"
                     alt="About"
-                    width={400}
-                    height={400}
+                    width={300}
+                    height={300}
                 />
             </div>
-            <div>
-                <h1 className="relative bottom-5 text-blue-500 text-3xl font-bold">
+            <div className="w-72 mx-auto">
+                <h1 className="relative bottom-5 md:text-left text-blue-500 text-4xl font-bold text-center">
                     About Me
                 </h1>
-                <p className="mt-2 text-md text-gray-600 text-align-left dark:text-white">
+                <p className="text-sm mt-2  md:text-left md:text-md text-gray-600 text-center dark:text-white md:w-72">
                     As a Computer Science graduate from Universidad Nueva
                     Esparta with over 3 years of working experience. Strong
                     foundation in technical skills and a passion for using
