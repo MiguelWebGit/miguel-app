0097587c5af74d26588afdf7c245ccc93e9deec0
Miguel Guerrero <miguel.dev.cloud@gmail.com>
2023-10-19 19:17:30 -0400

    hover effects on icons and projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,60 +1,60 @@
 import React from "react";
 import Image from "next/image";
-import {Project} from "./Project";
-import {FaGithub} from "react-icons/fa";
-import {IoIosOpen} from "react-icons/io";
+import { Project } from "./Project";
+import { FaGithub } from "react-icons/fa";
+import { IoIosOpen } from "react-icons/io";
 
 interface ProjectsProps {
-    projects: Project[];
+  projects: Project[];
 }
 
-const Projects: React.FC<ProjectsProps> = ({projects}) => {
-    return (
-        <div id="projects" className="text-center mb-20">
-            <div className="mx-auto w-80">
-                <h1 className="text-5xl text-blue-500 font-bold mb-10">
-                    Projects
-                </h1>
+const Projects: React.FC<ProjectsProps> = ({ projects }) => {
+  return (
+    <div id="projects" className="text-center mb-20">
+      <div className="mx-auto w-80">
+        <h1 className="text-5xl text-blue-500 font-bold mb-10">Projects</h1>
+      </div>
+      <div className="flex flex-col items-center md:flex-row md:justify-center md:flex-wrap">
+        {projects &&
+          projects.length > 0 &&
+          projects.map((project, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center mb-8 gap-8 w-full md:w-1/2"
+            >
+              <h2 className="text-2xl font-semibold mb-2">{project.title}</h2>
+              <Image
+                src={project.image}
+                alt={project.title}
+                className=""
+                width={200}
+                height={200}
+              />
+              <p className="text-base text-center mb-2 w-80">
+                {project.description}
+              </p>
+              <div className="flex justify-center space-x-4 ">
+                <a
+                  href={project.githubLink}
+                  className="flex items-center space-x-1 hover:scale-125 transition duration-300 ease-in-out"
+                >
+                  <FaGithub size={24} />
+                  <span>Code</span>
+                </a>
+
+                <a
+                  href={project.liveDemoLink}
+                  className="flex items-center space-x-1 hover:scale-125 transition duration-300 ease-in-out"
+                >
+                  <span>Live Demo</span>
+                  <IoIosOpen size={20} />
+                </a>
+              </div>
             </div>
-            <div className="flex flex-col items-center md:flex-row md:justify-center md:flex-wrap">
-                {projects &&
-                    projects.length > 0 &&
-                    projects.map((project, index) => (
-                        <div
-                            key={index}
-                            className="flex flex-col items-center mb-8 gap-8 w-full md:w-1/2">
-                            <h2 className="text-2xl font-semibold mb-2">
-                                {project.title}
-                            </h2>
-                            <Image
-                                src={project.image}
-                                alt={project.title}
-                                className=""
-                                width={200}
-                                height={200}
-                            />
-                            <p className="text-base text-center mb-2 w-80">
-                                {project.description}
-                            </p>
-                            <div className="flex justify-center space-x-4">
-                                <a
-                                    href={project.githubLink}
-                                    className="flex items-center space-x-1">
-                                    <FaGithub size={24} />
-                                    <span>Code</span>
-                                </a>
-                                <a
-                                    href={project.liveDemoLink}
-                                    className="flex items-center space-x-1">
-                                    <span>Live Demo</span>
-                                    <IoIosOpen size={20} />
-                                </a>
-                            </div>
-                        </div>
-                    ))}
-            </div>
-        </div>
-    );
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
